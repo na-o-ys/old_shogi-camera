d09b2702091266653940360a71736c298c087058
naoyoshi <naoyoshi0511@gmail.com>
2017-04-24 12:06:44 +0900

    a

---
+++ learn/read_koma.py
@@ -0,0 +1,27 @@
+import cv2
+import glob
+import numpy as np
+
+def display_cv_image(image, format='.png'):
+    decoded_bytes = cv2.imencode(format, image)[1].tobytes()
+    display(Image(data=decoded_bytes))
+
+# h * w のグレースケール
+def normalize(img, h, w):
+    size = img.shape[:2]
+    f = min(h / size[0], w / size[1])
+    resized = cv2.resize(img, (int(size[1] * f), int(size[0] * f)), interpolation=cv2.INTER_AREA)
+    
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    blank = np.full((h, w), np.uint8(255), dtype=np.uint8)
+    hstart = (h - gray.shape[0]) / 2
+    wstart = (w - gray.shape[1]) / 2
+    blank[hstart:(hstart + gray.shape[0]), wstart:(wstart + gray.shape[1])] = gray
+    return blank
+
+labels = ['gyoku', 'ou', 'kin', 'gin', 'kei', 'kyo', 'kaku', 'hi', 'fu', 'narigin', 'narikei', 'narikyo', 'uma', 'ryu', 'to']
+for label in labels:
+    files = glob.glob('../images/koma/%s/*' % label)
+    imgs = [cv2.imread(f) for f in files]
+    imgs = np.array([normalize(img, 32, 32) for img in imgs])
+    np.savez_compressed('npz/%s.npz' % label, imgs)
