3c54719bf345275f1642a9a407cb58dde9230984
naoyoshi <naoyoshi0511@gmail.com>
2017-04-24 11:03:50 +0900

    koma

---
+++ images/koma/index.rb
@@ -0,0 +1,17 @@
+def dir_files(dir)
+  Dir.glob(dir + "/*").map do |f|
+    "<img src='#{f}' height=30/>"
+  end
+end
+
+html = %w(<html> <body>)
+Dir.glob("./*").each do |dir|
+  next unless File.directory?(dir)
+  html.push("<div>")
+  html.push("<h4>#{File.basename(dir)}</h4>")
+  html += dir_files(dir)
+  html.push("</div>")
+end
+html += %w(</body> </html>)
+
+puts html
