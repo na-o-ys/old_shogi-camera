bf28a13d87ddc4aa57f4eb770485a1f70d589b66
naoyoshi <naoyoshi0511@gmail.com>
2017-05-02 17:38:01 +0900

    a

--- Makefile
+++ Makefile
@@ -10,9 +10,5 @@ build:
 	cd containers/gpu && docker build -t naoys/shogi-camera:latest .
 build-nogpu:
 	cd containers/nogpu && docker build -t naoys/shogi-camera:nogpu .
-extract_board:
-	$(DOCKER_CMD) run -v $(PWD):/app -it naoys/shogi-camera:$(DOCKER_TAG) python3 extract_board.py $(IMG)
-read_koma:
-	$(DOCKER_CMD) run -v $(PWD):/app -it naoys/shogi-camera:$(DOCKER_TAG) python3 learn/read_koma.py
 notebook:
 	$(DOCKER_CMD) run -v $(PWD):/app -p 8888:8888 -e "JUPYTER_CONFIG_DIR=/app/jupyter" -it naoys/shogi-camera:$(DOCKER_TAG) jupyter notebook --ip=0.0.0.0 --allow-root --no-browser
--- cli.py
+++ cli.py
@@ -5,8 +5,7 @@ import shogicam.data
 import shogicam.learn
 
 @click.group(help='shogi camera')
-@click.pass_context
-def main(ctx):
+def main():
     pass
 
 @main.command(help='Predict shogi board contents')
@@ -14,10 +13,41 @@ def main(ctx):
 def predict(ctx):
     print("predict")
 
+@main.command(help='Fit model')
+@click.option('--data-dir', '-d', type=click.Path(exists=True), default='data')
+@click.option('--outmodel-path', '-o', type=click.Path(), default='models/purple.h5')
+@click.option('--model', '-m', default='purple')
+def learn(data_dir, outmodel_path, model):
+    model = shogicam.learn.purple(data_dir, verbose=True)
+    shogicam.learn.save_model(model, outmodel_path)
+
+@main.group(help='Generate train data')
+def gen_traindata():
+    pass
+
+@gen_traindata.command(help='Generate koma train data')
+@click.option('--img-dir', '-i', type=click.Path(exists=True), default='images/koma')
+@click.option('--outdata-dir', '-o', type=click.Path(exists=True), default='data/koma')
+def koma(img_dir, outdata_dir):
+    result = shogicam.data.gen_koma_traindata(img_dir, outdata_dir)
+    print(result)
+
+@gen_traindata.command(help='Generate empty cell train data')
+@click.option('--img-dir', '-i', type=click.Path(exists=True), default='images/empty_cell')
+@click.option('--outdata-path', '-o', type=click.Path(), default='data/empty_cell.npz')
+def empty_cell(img_dir, outdata_path):
+    result = shogicam.data.gen_empty_cell_traindata(img_dir, outdata_path)
+    print(result)
+
+@main.command(help='Predict cell contents')
+@click.pass_context
+def predict_cell(ctx):
+    print("predict-cell")
+
 @main.command(help='Detect corner coordinates')
 @click.argument('img_path', type=click.Path(exists=True))
 @click.option('--out-img-path', '-o', type=click.Path(), default=None)
-def corners(img_path, out_img_path):
+def detect_corners(img_path, out_img_path):
     raw_img = shogicam.util.load_img(img_path)
     rect, score = shogicam.corners(raw_img)
     if out_img_path:
@@ -34,32 +64,6 @@ def corners(img_path, out_img_path):
         "score": score
     }
     print(decoded)
-
-@main.command(help='Predict cell contents')
-@click.pass_context
-def predict_cell(ctx):
-    print("predict-cell")
-
-@main.command(help='Generate koma train data')
-@click.argument('img_dir', type=click.Path(exists=True), default='images/koma')
-@click.argument('outdata_dir', type=click.Path(exists=True), default='data/koma')
-def gen_koma_traindata(img_dir, outdata_dir):
-    result = shogicam.data.gen_koma_traindata(img_dir, outdata_dir)
-    print(result)
-
-@main.command(help='Generate empty cell train data')
-@click.argument('img_dir', type=click.Path(exists=True), default='images/empty_cell')
-@click.argument('outdata_path', type=click.Path(), default='data/empty_cell.npz')
-def gen_empty_cell_traindata(img_dir, outdata_path):
-    result = shogicam.data.gen_empty_cell_traindata(img_dir, outdata_path)
-    print(result)
-
-@main.command(help='Fit model')
-@click.option('--data-dir', '-d', type=click.Path(exists=True), default='data')
-@click.option('--outmodel-path', '-o', type=click.Path(), default='model.h5')
-def learn(data_dir, outmodel_path, model='purple'):
-    model = shogicam.learn.purple(data_dir, verbose=True)
-    shogicam.learn.save_model(model, outmodel_path)
 
 if __name__ == '__main__':
     main()
