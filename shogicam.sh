62e324c371860d0f1b6756e8ef992ee335b0dc08
Ubuntu <ubuntu@ip-172-31-42-50.ec2.internal>
2017-05-02 03:14:43 +0000

    a

--- shogicam.sh
+++ shogicam.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker run -it -v $PWD:/app naoys/shogi-camera:nogpu python3 cli.py $@
+if [ -z "$NOGPU" ]; then
+  DOCKER_TAG=latest
+  DOCKER_CMD=nvidia-docker
+else
+  DOCKER_TAG=nogpu
+  DOCKER_CMD=docker
+fi
+
+$DOCKER_CMD run -it -v $PWD:/app naoys/shogi-camera:$DOCKER_TAG python3 cli.py $@
